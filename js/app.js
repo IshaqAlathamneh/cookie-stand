4a21a7277f66cd6cd3c951660b06f547418e6c70
IshaqAlathamneh <ishaqalathamneh@gmail.com>
2021-02-17 20:08:08 +0200

    finish

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 'use stricket';
 let mainEl = document.getElementById('the main')
 let tableEl = document.createElement('table')
+let countRaw=1;
 mainEl.appendChild(tableEl)
 let tableH = function(){
     let tr = document.createElement('tr')
@@ -14,7 +15,9 @@ let tableH = function(){
     th2.textContent = hourArr[i]
     
 } }
-
+// 'seattle', 'tokyo', 'dubai', 'paris', 'lima'
+// let allStores = ['seattle', 'tokyo', 'dubai', 'paris', 'lima']
+let totArr = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 let hourArr = ['6:00am:','7:00am:','8:00am:','9:00am:','10:00am:','11:00am:','12:00pm:','1:00pm:','2:00pm:','3:00pm:','4:00pm:','5:00pm:','6:00pm:','7:00pm:','Daily Total:'];
 function Stores(location,maximum,minimum,avg){
     this.locFac = location;
@@ -22,8 +25,10 @@ function Stores(location,maximum,minimum,avg){
     this.minHoCu = minimum;
     this.avgCookieCu = avg;
     this.cookiePerHo = [];
-
+    this.total = 0;
+ 
 }
+let nt1=0 , nt2=0;
 Stores.prototype.pushing = function(){
     let a = 0 , b = 0;
     for( let i = 0 ; i < hourArr.length ; i++){
@@ -33,13 +38,17 @@ Stores.prototype.pushing = function(){
        
        if (i < hourArr.length - 1 ){ 
            this.cookiePerHo.push(a);
+           totArr[i]+=this.cookiePerHo[i]
+           
      
    }else if (i === hourArr.length - 1){ 
+      
        this.cookiePerHo.push(b);
-       
+       totArr[i]+=this.cookiePerHo[i]
  }
 }}
 Stores.prototype.render = function(){
+    countRaw++;
  let trEl = document.createElement('tr')
  tableEl.appendChild(trEl)
  let td1 = document.createElement('td')
@@ -72,16 +81,38 @@ function getRandomInt(min, max) {
   lima.pushing();
   lima.render();
   
-  let tableF = function(){
+  function tableF(){
+    let a=0;
     let tr = document.createElement('tr')
     tableEl.appendChild(tr)
     let th1 = document.createElement('th')
     tr.appendChild(th1)
     th1.textContent = 'Totals'
-    for ( let i = 0; i <seattle.cookiePerHo.length ; i++){
-    let th2 = document.createElement('th')
-    tr.appendChild(th2)
-    th2.textContent = seattle.cookiePerHo[i]+tokyo.cookiePerHo[i]+dubai.cookiePerHo[i]+paris.cookiePerHo[i]+lima.cookiePerHo[i]
-   
-  }} 
+    for ( let i = 0; i <totArr.length ; i++){
+        
+        let th2 = document.createElement('th')
+        tr.appendChild(th2)
+        th2.textContent = totArr[i];
+    }
+} 
+tableF()
+ 
+  let stores = document.getElementById('my stores')
+  stores.addEventListener('submit',storesCreator)
+  function storesCreator(event){
+  event.preventDefault()
+  let storeLocation = event.target.locfac.value;
+//   console.log(event.target.locfac.value)
+  let maxCH = event.target.maxCH.value;
+  parseInt(maxCH)
+  let minCH = event.target.minCH.value;
+  parseInt(minCH)
+  let avgCC = event.target.avgCC.value;
+  parseFloat(avgCC)
+  
+  let newStore = new Stores(storeLocation, maxCH, minCH,avgCC);
+  tableEl.deleteRow(countRaw);
+  newStore.pushing()
+  newStore.render()
   tableF()
+} 
